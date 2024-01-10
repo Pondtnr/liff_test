b4a9a56b600958ecb26eced5a3042f249cdd3134
Pondtnr <pondpond709@gmail.com>
2024-01-10 15:51:33 +0700

    init project

--- src/views/Coupon.vue
+++ src/views/Coupon.vue
@@ -1,34 +1,31 @@
 <template>
-  <div class="surface-section px-4 py-3 md:px-6 lg:px-8">
+  <div class="surface-section px-4 py-3 md:px-6 lg:px-2">
     <div style="display: flex; align-items: center;">
       <i class="pi pi-ticket mr-2" style="font-size: 24px;"></i>
       <p style="margin: 0; font-size: 20px;">My Coupon</p>
     </div>
     <div class="grid py-2">
-
       <div class="col-12 md:col-4 flex flex-column align-items-center mb-3 md:mb-0">
         <div class="flex align-items-center">
-          <img src="@/assets/coupon.png" width="180"
+          <img src="@/assets/coupon.png" width="200"
             class="inline-flex align-items-center justify-content-center bg-cyan-500 mb-4">
-          <span class="text-900 text-xl font-medium mb-3">Discount 30%
-            <p class="text-600 line-height-3 py-0 m-0 px-3" style="font-size:15px;">SOLAOบ้านเจ๊...</p>
-          </span>
+          <i class="text-900 text-xl font-medium mb-3 px-3 ">Discount 30%
+            <button>GET</button></i>
         </div>
         <div class="flex align-items-center">
-          <img src="@/assets/coupon.png" width="180"
+          <img src="@/assets/coupon.png" width="200"
             class="inline-flex align-items-center justify-content-center bg-cyan-500 mb-4">
-          <span class="text-900 text-xl font-medium mb-3">Discount 30%
-            <p class="text-600 line-height-3 py-0 m-0 px-3" style="font-size:13px;">SOLAOคันคลอง...</p>
-          </span>
+          <i class="text-900 text-xl font-medium mb-3 px-3">Discount 30%
+            <button>GET</button>
+          </i>
         </div>
         <div class="flex align-items-center">
-          <img src="@/assets/coupon.png" width="180"
+          <img src="@/assets/coupon.png" width="200"
             class="inline-flex align-items-center justify-content-center bg-cyan-500 mb-4">
-          <span class="text-900 text-xl font-medium mb-3">Discount 30%
-            <p class="text-600 line-height-3 py-0 m-0 px-3" style="font-size:15px;">SOUELMIND...</p>
-          </span>
+          <i class="text-900 text-xl font-medium mb-3 px-3">Discount 30%
+            <button>GET</button>
+          </i>
         </div>
-
       </div>
     </div>
   </div>
